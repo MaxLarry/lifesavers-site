3c77e265f48af082b48f6599296941d6996ed50a
MaxLarry <larryjohnandonga.dev@gmail.com>
2025-07-21 08:35:00 +0800

    roxas barbacan

--- src/app/(routes)/(book)/book/page.tsx
+++ src/app/(routes)/(book)/book/page.tsx
@@ -17,13 +17,9 @@ const Book = () => {
   const router = useRouter();
 
   const availableDates = [
-    new Date(2025, 6, 8),
-    new Date(2025, 6, 9),
-    new Date(2025, 6, 10),
-    new Date(2025, 6,11),
-     new Date(2025, 6,14),
-      new Date(2025, 6,15),
-       new Date(2025, 6,16),
+    new Date(2025, 6, 21),
+    new Date(2025, 6,22),
+    new Date(2025, 6, 23),
   ];
 
   const availableTimes = [
@@ -146,7 +142,7 @@ const Book = () => {
           </p>
           <p className="medium text-white mt-3">
             <span className="text-red-600 font-black">Note: </span>The available
-            dates are for <strong>ROXAS PALAWAN</strong> (July 9-11 & July 14-16) venue only. For
+            dates are for <strong>ROXAS PALAWAN</strong> (July 21-23) venue only. For
             updates, please follow our{" "}
             <a
               href="https://www.facebook.com/profile.php?id=100054367578630"
